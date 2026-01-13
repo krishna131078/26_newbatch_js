f7603fd4185ab9974b390cf92ee55855131df33e
krishna131078 <krishna131078@gmail.com>
2026-01-13 19:52:19 +0530

    operators

---
+++ opertors/operator2.js
@@ -0,0 +1,10 @@
+let x=10
+let y=20
+x+=10
+console.log(x)
+y-=10
+console.log(y)
+y*=2
+console.log(y)
+x/=2
+console.log(x)
---
+++ opertors/opertaor1.js
@@ -0,0 +1,7 @@
+let a=100
+let b=20
+console.log(a+b)//addition
+console.log(a-b)//sub
+console.log(a*b)//multi
+console.log(a/b)//division
+console.log(a%b)//mod
