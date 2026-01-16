61ba3015acdb414e788210c11e82cba97d2fc21b
krishna131078 <krishna131078@gmail.com>
2026-01-16 18:02:13 +0530

    ifelse

--- ifelse/ifelse1.js
+++ ifelse/ifelse1.js
@@ -15,11 +15,76 @@
 // else{
 //     console.log("no is odd")
 // }
-var num=2
-if(num%5==0 || num%7==0)
+// var num=2
+// if(num%5==0 || num%7==0)
+// {
+//     console.log("no is divisble by both")
+// }
+// else{
+//     console.log("no is not divisible")
+// }
+
+// var num= -3
+// if(num>0)
+// {
+//     console.log("no is pos")
+// }
+// else if(num<0)
+// {
+//     console.log("no is neg")
+// }
+// else{
+//     console.log("no is zero")
+// }
+// var a=10
+// var b=10
+// if(a>b)
+// {
+//     console.log("a is max")
+// }
+// else if(b>a)
+// {
+//     console.log("b is max")
+// }
+// else{
+//     console.log("both are equal")
+// }
+// var marks=70
+// if(marks>=90)
+// {
+//     console.log("grade a")
+// }
+// else if(marks>=80 && marks<90)
+// {
+//     console.log("grade b")
+// }
+// else if(marks>=70 && marks<=80)
+// {
+//     console.log("grade c")
+// }
+// else{
+//     console.log("Fail")
+// }
+
+var a=40
+var b=20
+var c=50
+if(a>b)
 {
-    console.log("no is divisble by both")
+    if(a>c)
+    {
+        console.log("a is max")
+    }
+    else{
+        console.log("c is max")
+    }
 }
 else{
-    console.log("no is not divisible")
+    if(b>c)
+    {
+        console.log("b is macx")
+    }
+    else{
+        console.log("c is max")
+    }
 }
