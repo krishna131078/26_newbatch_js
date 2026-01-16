f1995213ed11eba8eae18e21a12452833ae7dd9b
krishna131078 <krishna131078@gmail.com>
2026-01-16 17:39:36 +0530

    ifelse

---
+++ ifelse/ifelse1.js
@@ -0,0 +1,25 @@
+// var num=2
+// if(num>0)
+// {
+//     console.log("no is pos")
+// }
+// else
+// {
+//     console.log('no is negative')
+// }
+// num=parseInt(prompt("enter the num"))
+// if (num%2==0)
+// {
+//     console.log("no is even")
+// }
+// else{
+//     console.log("no is odd")
+// }
+var num=2
+if(num%5==0 || num%7==0)
+{
+    console.log("no is divisble by both")
+}
+else{
+    console.log("no is not divisible")
+}
---
+++ opertors/operator3.js
@@ -0,0 +1,6 @@
+// ==,!=,<,>,<=,>=
+var num=10
+var num2=12
+console.log(num>num2)
+console.log(num==num2)
+console.log(num!=num2)
