de75db84afec83c1da7cd3c4112b482f875dbc2d
krishna131078 <krishna131078@gmail.com>
2026-01-21 18:42:48 +0530

    for loop

---
+++ loops/loops1.js
@@ -0,0 +1,56 @@
+// for lopp--->entry control
+// 3.
+// 1.init
+// 2.condition
+// 3.incre/decre
+
+// for(let i=1;i<=10;i++)
+// {
+//     console.log(i)//1//2//3
+// }
+// for(let i=10;i>=1;i--)
+// {
+//     console.log(i)//10//9//
+// }
+// var sum=0
+// for(let i=1;i<=10;i++)
+// {
+//     console.log(i)
+//     sum+=i
+    
+// }
+// console.log(sum)
+// var osum=0
+// var esum=0
+// for(let i=1;i<=10;i++)
+// {
+//     if(i%2==0)
+//     {
+//         esum=esum+i
+//     }
+//     else{
+//         osum=osum+i
+//     }
+// }
+// console.log(esum)
+// console.log(osum)
+
+// 5---->5*4*3*2*1=120
+//4--->4*3*2*1=24
+// var num=5
+// var fact=1
+// for(let i=1;i<=num;i++)
+// {
+//     fact=fact*i//1*1=1//1*2=2//2*3=6//6*4=24//5*24//120
+// }
+// console.log(fact)
+// 2--->3=8
+var base=2
+var power=3
+var m=1
+for(let i=1;i<=power;i++)
+
+{
+    m=m*base//1*2=2//2*2=4//4*2=8
+}
+console.log(m)
