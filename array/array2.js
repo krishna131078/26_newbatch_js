4d6512587d05d3503d0548f047748a1ffba12f25
krishna131078 <krishna131078@gmail.com>
2026-02-11 18:53:08 +0530

    array

--- array/array2.js
+++ array/array2.js
@@ -14,19 +14,33 @@ var upper=[]
 // });
 // map
 //array
-upper=data.map((e)=>{
-    return e.toUpperCase()
-})
-console.log(upper)
+// upper=data.map((e)=>{
+//     return e.toUpperCase()
+// })
+// console.log(upper)
 
 
-lower=data.map((element)=>{
-    return element.toLowerCase()
-})
-console.log(lower)
+// lower=data.map((element)=>{
+//     return element.toLowerCase()
+// })
+// console.log(lower)
 
-var arr=[5,10,20]
+// var arr=[5,10,20]
 
 
-j=arr.map((e)=>e>8)
-console.log(j)
+// j=arr.map((e)=>e>8)
+// console.log(j)
+// data.forEach(element => {
+//     upper.push(element)
+    
+// });
+// console.log(upper)
+// var arr=['a','b','c']
+// var result=arr.map((i,x)=>x+' '+i)
+// console.log(result)
+let arr=[1,2,3,4]
+let res=arr.map((e)=>e*2)
+console.log(res)
+
+let arr1=['jay','dhyey','naman']
+let arr2=[]
