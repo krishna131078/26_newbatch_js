63c8f9e996c1ed4d9ed0ddd13950fbad20cf5c13
krishna131078 <krishna131078@gmail.com>
2026-02-11 18:07:19 +0530

    array

---
+++ array/array1.js
@@ -0,0 +1,25 @@
+var data=[10,23,42,78,13]
+// console.log(data[0])
+// console.log(data[1])
+
+var max=data[0]
+for(let i=0;i<data.length;i++)
+{
+    // console.log(data[i])//
+    if(data[i]>max)
+    {
+        max=data[i]//10>23//23//42//78
+    }
+
+}
+console.log(max)
+
+var sum=0
+for(i=0;i<data.length;i++)
+{
+    sum=sum+data[i]//0+10//10+23=33//
+}
+console.log(sum)
+
+// 
+// var data=["ram","amit","kunal","shyam","geeta","sita"]---->
--- string/string1.js
+++ string/string1.js
@@ -32,3 +32,27 @@ var x=data.toUpperCase()
 console.log(x)
 var x=data.toLowerCase()
 console.log(x)
+
+var data=" hi hello"
+var data2=data.startsWith(" ")
+var data3=data.endsWith("l")
+var isexist=data.includes("z")
+console.log(data2)
+console.log(data3)
+console.log(isexist)
+
+var data=" hi hello "
+var center=data.trim()
+console.log(center)
+console.log(data.length)
+console.log(center.length)
+var left=data.trimStart()
+console.log(left)
+console.log(left.length)
+var right=data.trimEnd()
+console.log(right)
+// console.log(first)
+console.log(right.length)
+var s="royal technosoft"
+console.log(s)
+
