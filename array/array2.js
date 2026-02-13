410a33a33421d50b41cd830ba8f8b509591cb1cb
krishna131078 <krishna131078@gmail.com>
2026-02-13 18:09:38 +0530

    flatmap

--- array/array2.js
+++ array/array2.js
@@ -38,24 +38,68 @@ var upper=[]
 // var arr=['a','b','c']
 // var result=arr.map((i,x)=>x+' '+i)
 // console.log(result)
-let arr=[1,2,3,4]
-let res=arr.map((e)=>e*2)
-console.log(res)
+// let arr=[1,2,3,4]
+// let res=arr.map((e)=>e*2)
+// console.log(res)
 
-let arr1=['jay','dhyey','naman']
-let arr2=[]
+// let arr1=['jay','dhyey','naman']
+// let arr2=[]
 
-for(let i=0;i<arr1.length;i++)
-{
-    console.log(arr1[i].split("").reverse().join(""))
+// for(let i=0;i<arr1.length;i++)
+// {
+//     console.log(arr1[i].split("").reverse().join(""))
 
-    if(arr1[i].split("").reverse().join("")==arr1[i])
-    {
-        console.log(arr2.push(arr1[i]))
-    }
-}
+//     if(arr1[i].split("").reverse().join("")==arr1[i])
+//     {
+//         console.log(arr2.push(arr1[i]))
+//     }
+// }
 //n a m a n
 //naman
 //j a y
 // y a j
 //
+// let student=[
+//     {name:"jay",age:22,id:1},
+//     {name:"dhyey",age:23,id:2}
+// ]
+// let result=student.map((stu)=>{
+//     return {
+//         name:stu.name,
+//         age:stu.age,
+//         id:stu.id
+
+//     }
+// })
+
+// console.log(result)
+// let arr=[10,25,30,45]
+// // let res=arr.map((a)=>a*a)
+// // console.log(res)
+// let res=arr.filter((a)=>a%2==0)
+// console.log(res)
+// let li=["ram","shyam","amit","aarav"]
+// let result=li.filter((user)=>user.startsWith("a"))
+// console.log(result)
+// let arr=[
+//     {name:"kunal",age:22,marks:40},
+//     {name:"amit",age:23,marks:90}
+// ]
+// let res=arr.filter((user)=>user.marks>40)
+// console.log(res)
+
+// var data=[["sachin","kohli"],["rohit","raina"],["gill","pandya"]]
+// var filterdata=data.map((user)=>{
+//     return user.filter((d)=>{
+//         return d.length==6
+//     })
+// })
+// console.log(filterdata)
+var data=[["sachin","kohli"],["rohit","raina"],["gill","pandya"]]
+var filterdata=data.flatMap((user)=>{
+    // return user
+    return user.filter((d)=>{
+        return d.length==6
+    })
+})
+console.log(filterdata)
